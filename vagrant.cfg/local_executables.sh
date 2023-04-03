265b0dfa78712cf393b7d3d7461372eca225c306
KOTP <keeperotphones@gmail.com>
2023-04-03 11:29:42 -0400

    Make directory or do not, but do not error

--- vagrant.cfg/local_executables.sh
+++ vagrant.cfg/local_executables.sh
@@ -3,7 +3,7 @@
 echo "############################"
 echo "Setting up User executables."
 echo "############################"
-mkdir ~/bin/
+mkdir -p ~/bin/
 cp /vagrant/vagrant.cfg/serve_jekyll ~/bin/
 chmod +x ~/bin/serve_jekyll
 
