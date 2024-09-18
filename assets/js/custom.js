80a173aeefc4d46c8c5c46d0dc01473e2782a020
Dhanu Gurung <budhramgurung@gmail.com>
2024-09-18 08:43:38 +0545

    Use carpentries style in learning pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,30 @@
 /*Custom JavaScript*/
 $(function(){
+
+  // ------------- for learning pages
+  // Make all tables striped by default.
+  $(".learning-page table").addClass("table table-hover table-responsive");
+  $(".learning-page table thead").addClass("thead-dark");
+
+  // Handle foldable challenges and solutions (on click and at start).
+  $(".solution").click(function(event) {
+    var trigger = $(event.target).has(".fold-unfold").length > 0
+                || $(event.target).filter(".fold-unfold").length > 0;
+    if (trigger) {
+      $(">*:not(h2)", this).toggle(400);
+      $(">h2>span.fold-unfold", this).toggleClass("icon-collapse-down icon-collapse-up");
+      event.stopPropagation();
+    }
+  });
+
+  $(".solution").each(function() {
+    $(">*:not(h2)", this).toggle();
+    var h2 = $("h2:first", this);
+    h2.append("<span class='fold-unfold icon icon-collapse-down'></span>");
+  });
+  
+  // -------------------------------------------------------------------
+
   // lazy load images
   if(location.pathname != '/workshop-template') {
     const observer = lozad(); // lazy loads elements with default selector as '.lozad'
