43eecf91500315b0ceb094b2ef15542822639287
Gorkha Developer <gorkhadeveloper@gmail.com>
2024-10-08 23:08:26 +0545

    feat: added copy code to code blocks
    Fix #2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -120,4 +120,35 @@ $(function(){
     const newContent = targetTable.prop('outerHTML').replace("<table>", "<table class='table'>")
     targetTable.replaceWith("<div class='table-responsive'>" + newContent + "</div>");
   }
+
+  // copy code button
+  // Find all pre elements containing code elements
+  var pres = document.querySelectorAll(".highlight pre");
+  pres.forEach(function(pre) {
+    // Insert the copy button before the pre element
+    var button = createCopyButton(pre);
+    pre.parentNode.insertBefore(button, pre);
+  });
+
 });
+
+// Function to create a copy button
+function createCopyButton(pre) {
+  var button = document.createElement("button");
+  button.classList.add("copy-code")
+  button.innerHTML = '📝 Copy Code';
+  button.addEventListener("click", function() {
+      var code = pre.querySelector("code").innerText;
+      navigator.clipboard.writeText(code).then(function() {
+          button.innerText = "Copied!";
+          button.classList.add("copied");
+          setTimeout(()=>{
+              button.classList.remove("copied");
+              button.innerHTML = '📋 Copy Code';
+          },3000);
+      }, function(err) {
+          console.error("Could not copy text: ", err);
+      });
+  });
+  return button;
+}
